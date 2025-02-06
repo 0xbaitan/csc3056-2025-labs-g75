cc002bc19f79765a530462c5aab378e4ac766cb5
Tanish Baidya <tbaidya01@qub.ac.uk>
2025-02-06 11:20:26 +0000

    ✨  Add user class

--- SimpleBankingAppG75/src/model/User.java
+++ SimpleBankingAppG75/src/model/User.java
@@ -1,5 +1,62 @@
 package model;
 
+
+
 public class User {
-	private static final int TEST=12;
+	private String username;
+	private String password;
+	private String first_name;
+	private String last_name;
+	private String mobile_number;
+
+	// Constructor
+	public User(String username, String password, String first_name, String last_name, String mobile_number) {
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.mobile_number = mobile_number;
+	}
+
+	// Getters
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getFirstName() {
+		return first_name;
+	}
+
+	public String getLastName() {
+		return last_name;
+	}
+
+	public String getMobileNumber() {
+		return mobile_number;
+	}
+
+	// Setters
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public void setLastName(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public void setMobileNumber(String mobile_number) {
+		this.mobile_number = mobile_number;
+	}
 }
